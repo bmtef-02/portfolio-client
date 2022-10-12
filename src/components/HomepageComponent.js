5e27b42ba733f4b7dc35b6d2561722a6e54b9b28
Brian Jun <bmtef.02@gmail.com>
2022-10-12 13:22:42 -0700

    first commit

---
+++ src/components/HomepageComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Homepage() {
+
+    return (
+        <div className='d-flex justify-content-center'>
+            <h1>HOMEPAGE</h1>
+        </div>
+    );
+}
