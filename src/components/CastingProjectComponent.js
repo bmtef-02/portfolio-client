bb98cd4495aa8d4e0bbd52db5838c88da32869d3
Brian Jun <bmtef.02@gmail.com>
2022-10-13 18:13:39 -0700

    project routes added

--- src/components/CastingProjectComponent.js
+++ src/components/CastingProjectComponent.js
@@ -1,10 +1,94 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function CastingProject() {
 
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    })
+
     return (
-        <div>
-            Casting
-        </div>
+        <Container>
+            <Row className='justify-content-center' style={{ textAlign: 'center' }}>
+                <Col xs={12} lg={8}>
+                    <h1>Casting Cards App</h1>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <Row style={{ textAlign: 'center' }}>
+                        <Col xs={4}>
+                            <h4>
+                                <a href='https://casting-card-client.netlify.app/' target='_blank'>Live</a>
+                            </h4>
+                        </Col>
+                        <Col xs={4}>
+                            <h4>
+                                <a href='https://github.com/bmtef-02/casting-cards-react-to-print.git' target='_blank'>GitHub Frontend</a>
+                            </h4>
+                        </Col>
+                        <Col xs={4}>
+                            <h4>
+                                <a href='https://github.com/bmtef-02/casting-cards-server.git' target='_blank'>GitHub Backend</a>
+                            </h4>
+                        </Col>
+                    </Row>
+                    
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Background</h2>
+                    <p>In the TV industry, the casting department will pitch contestants to network executives to cast the show. As a visual aid, network executives like to see contestant cards laid out in a grid. Each contestant card includes their headshot and information. These cards are used to help make decisions on the final cast. The software currently used by casting teams can create cards, but they are printed individually which take up an entire page. Imagine the amount of paper wasted when pitching hundreds of contestants. Aside from the environmental concerns, the current software cannot print a one-pager of cards laid out in a grid. Therefore, casting departments manually create these grids using a word processor. Many hours can be spent sizing each card, cropping every headshot, and copying and pasting contestant information from a database.</p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Goal</h2>
+                    <p>I created this website to be a tool casting departments can use to create a grid of cards more efficiently. To save time with formatting, users select whether to fit 8 or 10 cards on one page, and a 4x2 or 5x2 grid template is created. To fill the grid with data, users simply select a contestant’s name from a dropdown menu and their headshot and other information are automatically added. Once all contestants are selected, users can save the grid for future use, print the grid, or download the grid as a PDF. </p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Technologies</h2>
+                    <ul>
+                        <li>React.js</li>
+                        <li>Express.js</li>
+                        <li>Node.js</li>
+                        <li>Axios</li>
+                        <li>React Router</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                        <li>JavaScript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Bootstrap</li>
+                        <li>React-Bootstrap</li>
+                    </ul>
+                    <p><b>Backend:</b> Express.js was used to create the backend, including all the routes. Mongoose was used to create the schema for the saved templates and the contestants.</p>
+                    <p><b>Frontend:</b> React and React Router were used to create a single page application. JSX was used to create templates, populate contestant information, process forms, and send search queries to the backend. React-Bootstrap, Bootstrap, and CSS were used for styling.</p>
+                    <p><b>Database:</b> MongoDB was used for the database to store the saved templates and the list of contestants users can select from. Axios was used to make requests from the browser.</p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Features</h2>
+                    <p>Upon opening the application, users are greeted with a homepage that briefly describes the purpose and functionality of the app. From the homepage, users can either create a grid or view saved grids.</p>
+                    <p>Users have the choice to create a 4x2 grid that shows 8 cards per page, or a 5x2 grid that shows 10 cards per page. To fill a grid with contestant data, users select a contestant from a dropdown menu which doubles as a search bar. By default, the dropdown menu lists contestants by last name A-Z, but users can sort contestants by last name Z-A, ascending or descending age, or ethnicity A-Z. There are plus and minus buttons in the bottom right corner that users can click on to add or subtract pages.</p>
+                    <p>Once a user selects all their contestants, they can save the grid to view or edit later. By clicking save, a modal appears and users are asked to enter the show name, season, pitch/round number, and an optional description. A red notification badge appears on the save button if changes to the grid have not been saved by the user. This functionality is included to notify the user that if they close out of the app without saving, their grid will be deleted.</p>
+                    <p>When users want to print their grid they click on the Preview PDF button. This will show them a preview of their grid. Clicking on the Print/Save PDF button will open the browser printing menu where users can send the grid to their printer or save the grid as a PDF to their device. The Printing Tips button expands a menu that displays tips for best printing results.</p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Lessons Learned</h2>
+                    <p>I learned the importance of researching different libraries and components that you can use to create better functionality in your app. React-Select is an example of a component I use as a selectable menu but also a searchable list. Creating this component on my own would have been extremely difficult.</p>
+                    <p>ReactToPrint is another component I use in my app to give users the ability to print out their grids in a consistently styled format. Without this component, grids would not be perfectly printed on A4-sized paper and in landscape orientation. Because I use various components, I learned the importance of being able to read documentation and read the issues that users post to the component’s GitHub to solve problems of my own.</p>
+                    <p>Finally, I greatly improved my knowledge of Flexbox while styling the app to be responsive. It was tedious to get the spacing and positioning of various components the way I envisioned, especially at different breakpoints. Once again, I learned how important it is to be able to read and implement documentation.</p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
--- src/components/RecipeProjectComponent.js
+++ src/components/RecipeProjectComponent.js
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function RecipeProject() {
 
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    })
+
     return (
-        <div>
-            Recipe
-        </div>
+        <Container>
+            <Row className='justify-content-center' style={{ textAlign: 'center' }}>
+                <Col xs={12} lg={8}>
+                    <h1>Recipe App</h1>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <Row style={{ textAlign: 'center' }}>
+                        <Col xs={4}>
+                            <h4>
+                                <a href='https://bmtef-02-recipe-app.netlify.app/' target='_blank'>Live</a>
+                            </h4>
+                        </Col>
+                        <Col xs={4}>
+                            <h4>
+                                <a href='https://github.com/bmtef-02/recipe-app-client.git' target='_blank'>GitHub Frontend</a>
+                            </h4>
+                        </Col>
+                        <Col xs={4}>
+                            <h4>
+                                <a href='https://github.com/bmtef-02/recipe-app-server.git' target='_blank'>GitHub Backend</a>
+                            </h4>
+                        </Col>
+                    </Row>
+                    
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Background</h2>
+                    <p>Searching for and reading recipes online is such a pain. When you find a recipe, time is wasted scrolling through blog posts and advertisements until you finally reach what you actually need: the ingredients and directions. Saving these recipes to a notes app or a spreadsheet makes the experience even worse. Imagine how tedious it must be to click open links and sift through meaningless information just to see if you have the correct ingredients to make dinner.</p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Goal</h2>
+                    <p>I created this website to be a minimalist and customizable recipe app that allows users to create and save their own recipes. Searching for and filtering through recipes is easy, too. Users can search by recipe name or filter by category. Have extra tomatoes lying around your kitchen and don’t know what to make with them? Users can also search by ingredient to see what they could cook for dinner.</p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Technologies</h2>
+                    <ul>
+                        <li>React.js</li>
+                        <li>Express.js</li>
+                        <li>Node.js</li>
+                        <li>Axios</li>
+                        <li>React Router</li>
+                        <li>MongoDB</li>
+                        <li>Mongoose</li>
+                        <li>JavaScript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Material UI</li>
+                        <li>Bootstrap</li>
+                        <li>React-Bootstrap</li>
+                    </ul>
+                    <p><b>Backend:</b> Express.js was used to create the backend, including all the routes. Mongoose was used to create the schema for the recipes.</p>
+                    <p><b>Frontend:</b> React and React Router were used to create a single page application. JSX was used to populate search results, process the forms, and send queries to the backend. React-Bootstrap, Bootstrap, and CSS were used for styling.</p>
+                    <p><b>Database:</b> MongoDB was used for the database to store the saved recipes. Axios was used to make requests from the browser.</p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Features</h2>
+                    <p>Upon opening the application, users choose to create a recipe or search for one. If users create a recipe, they fill out a form and save it to a database. The form includes fields for the name, ingredients, directions, tags, and optional notes. When users are listing out the ingredients and directions, they can add or remove as many entries as they want by clicking the plus or minus buttons. Users can add tags to recipes to categorize them by choosing from a default list or creating a new tag. Before the recipe is saved, the form checks for validation on the name, ingredients, directions and tag fields.</p>
+                    <p>Users can search for a recipe by recipe name or an ingredient. All recipes that include the search query are populated on the search results page. Users can also filter by tags using a select menu. When the user finds the desired recipe, they click on the recipe card and are routed to the recipe page that displays all the relevant information. If users want to edit the recipe, they can enable the form by clicking the edit recipe button.</p>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} lg={8}>
+                    <h2>Lessons Learned</h2>
+                    <p>One lesson I learned was the importance of maintaining state in an organized way. Some challenges I faced were handling field changes to update state and validating the form on submission. The useState hook allowed me to easily maintain state and check for validation by passing props to child components. While passing state and updater functions as props works well for a few input fields, organization can get difficult when dealing with many input fields. In the future, I plan on implementing the useContext hook or Redux to maintain state.</p>
+                    <p>Another challenge I faced was creating a component that would allow users to add their own options to a select menu if the default options don’t include what they want. The Material UI Autocomplete component met my needs perfectly. At first it was tricky to understand the different syntax, but I was able to successfully implement the component. I learned how important it is to be able to read documentation, and I plan on using Material UI for styling in future projects.</p>
+                </Col>
+            </Row>
+        </Container>
     );
 }
